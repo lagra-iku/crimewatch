2d964decbf50a96f0637c070ad93cbfd4261ee8f
Grace O. Nweke <lagra2001@gmail.com>
2024-05-28 15:06:29 +0100

    Resolving URLs to their views

--- app/urls.py
+++ app/urls.py
@@ -3,8 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.app, name='app'),
+    path('case/new/', views.case_new, name='case_new'),
+    path('register_officer', views.register_officers, name='register_officers'),
     path('list', views.case_list, name='case_list'),
     path('case/<int:pk>/', views.case_detail, name='case_detail'),
-    path('case/new/', views.case_new, name='case_new'),
+    
     path('case/<int:pk>/edit/', views.case_edit, name='case_edit'),
 ]
